59056f63649da6a5dda43529f6b1e40d71d49593
mmmm <121663016@qq.com>
2018-10-19 22:34:13 +0800

    daily commit

---
+++ app/src/main/java/me/unknow/noname/di/ActivityComponent.java
@@ -0,0 +1,9 @@
+package me.unknow.noname.di;
+
+import dagger.Component;
+
+@Component(modules = ActivityModule.class)
+public class ActivityComponent {
+
+
+}
---
+++ app/src/main/java/me/unknow/noname/di/ActivityModule.java
@@ -0,0 +1,9 @@
+package me.unknow.noname.di;
+
+import dagger.Module;
+
+@Module
+public class ActivityModule {
+
+
+}
